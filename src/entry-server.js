9f941ffdf01f7564fcb86e7cfa0a20ba426f55a7
unknown <YJN>
2021-09-25 03:09:44 +0900

    Vue SSR 적용

---
+++ src/entry-server.js
@@ -0,0 +1,17 @@
+import { createApp } from './main';
+
+export default (ctx) => {
+	return new Promise((resolve, reject) => {
+		const { app, router, store } = createApp();
+    // 서버쪽 URL 푸쉬
+		router.push(ctx.url);
+		router.onReady(() => {
+			ctx.rendered = () => ctx.state = store.state;
+			const matchedComponents = router.getMatchedComponents();
+			if (matchedComponents.length === 0) {
+				return reject({ code: 404 });
+			}
+			return resolve(app);
+		}, reject);
+	});
+}
